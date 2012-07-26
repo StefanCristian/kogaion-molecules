094f97666b66400091dc6fa85b2c9cf9d579a29f
Steven Cristian <stevencristian@hotmail.com>
2012-07-27 02:37:21 +0300

    [remaster posh sh] deleted sequential not needed

--- scripts/remaster_post.sh
+++ scripts/remaster_post.sh
@@ -5,8 +5,6 @@ CHROOT_PKGS_DIR="${CHROOT_DIR}/var/lib/entropy/client/packages"
 # load common stuff
 . /sabayon/scripts/remaster_post_common.sh
 
-cp /sabayon/remaster/rogentos/xfce-configs/xfce4-desktop.xml ${CHROOT_DIR}/home/rogentosuser/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
-
 # make sure to not leak /proc
 umount "${CHROOT_DIR}/proc" &> /dev/null
 umount "${CHROOT_DIR}/proc" &> /dev/null
