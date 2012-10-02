b5916012d694ad0b168ec9623543f0aa166e3745
Stefan Cristian <steven.darklight@gmail.com>
2012-10-03 01:58:54 +0300

    reparat greselile pentru care nu boota nvidia proprietar pe legacy + adaugat alte optiuni si reparari la pachete. repaired nvidia proprietary + other fixes of unwanted installed programs

--- scripts/remaster_generic_inner_chroot_script_after_legacy.sh
+++ scripts/remaster_generic_inner_chroot_script_after_legacy.sh
@@ -287,9 +287,10 @@ if [ "$ARCH" = "x86_64" ]; then
 		equo unmask anaconda
 		equo install linux-sabayon:3.2 sabayon-sources:3.2 =x11-drivers/ati-userspace-11.12 =x11-drivers/ati-drivers-11.12#3.2.0-sabayon --nodeps
 		equo install =x11-drivers/nvidia-drivers-290.10#3.2.0-sabayon =x11-drivers/nvidia-userspace-290.10 =media-video/nvidia-settings-290.10 --nodeps
-		#equo install grub =x11-base/xorg-server-1.11.4
+		equo install grub #=x11-base/xorg-server-1.11.4
 		equo remove anaconda virtualbox-guest-additions:0,3.2.0-sabayon app-emulation/virtualbox-modules:0,3.2.0-sabayon net-wireless/broadcom-sta:0,3.2.0-sabayon net-wireless/madwifi-ng:0,3.2.0-sabayon net-wireless/ndiswrapper:0,3.2.0-sabayon xf86-video-virtualbox:0,3.2.0-sabayon nvidiabl:0,3.2.0-sabayon --nodeps
 		equo install app-admin/anaconda-9999 openrc --deep
+		depmod -a
 		echo -5 | equo conf update
 		equo remove linux-sabayon:3.3 linux-sabayon:3.5 --nodeps
 		env-update && source /etc/profile
@@ -304,7 +305,7 @@ equo mask ati-drivers
 equo mask nvidia-drivers
 equo mask ati-userspace
 equo mask nvidia-settings
-equo mask nvidia-userspace xorg-server nvidia-drivers nvidia-userspace virtualbox-guest-additions app-emulation/virtualbox-modules ndiswrapper net-wireless/broadcom-sta net-wireless/madwifi-ng nvidiabl xf86-video-virtualbox
+equo mask nvidia-userspace xorg-server nvidia-drivers nvidia-userspace virtualbox-guest-additions app-emulation/virtualbox-modules ndiswrapper net-wireless/broadcom-sta net-wireless/madwifi-ng nvidiabl xf86-video-virtualbox xorg-drivers
 echo "Interdict any kernel upgrade from now on, kernel-switcher only because it's Rogentos Legacy"
 }
 
