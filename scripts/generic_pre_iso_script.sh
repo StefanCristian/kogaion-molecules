092bbae15f42df59229b988a5a059abf5edefb48
BlackNoxis <steven.darklight@gmail.com>
2013-02-26 02:41:57 +0200

    Repaired the igz boot files

--- scripts/generic_pre_iso_script.sh
+++ scripts/generic_pre_iso_script.sh
@@ -13,6 +13,24 @@ grub_source="${ROGENTOS_MOLECULE_HOME}/remaster/minimal_grub.cfg"
 isolinux_destination="${CDROOT_DIR}/isolinux/txt.cfg"
 grub_destination="${CDROOT_DIR}/boot/grub/grub.cfg"
 
+rm "${CDROOT_DIR}/autorun.inf"
+rm "${CDROOT_DIR}/sabayon.ico"
+rm "${CDROOT_DIR}/sabayon.bat"
+echo "Moving the right files where they rightfully belong"
+cp /sabayon/boot/core/autorun.inf "${CDROOT_DIR}/"
+cp /sabayon/boot/core/rogentos.ico "${CDROOT_DIR}/"
+cp /sabayon/boot/core/rogentos.bat "${CDROOT_DIR}/"
+echo "Copying them into the ISO image"
+
+if [ -d "/home/rogentosuser/.gvfs" ]; then
+        echo "All is doomed"
+        umount /home/rogentosuser/.gvfs
+        rm -r /home/rogentosuser/.gvfs
+fi
+
+mv "${CHROOT_DIR}/boot/sabayon.igz" "${CHROOT_DIR}/boot/rogentos.igz"
+mv "${CHROOT_DIR}/boot/sabayon" "${CHROOT_DIR}/boot/rogentos"
+
 if [ "${remaster_type}" = "KDE" ] || [ "${remaster_type}" = "GNOME" ]; then
 	isolinux_source="${ROGENTOS_MOLECULE_HOME}/remaster/standard_isolinux.cfg"
 	grub_source="${ROGENTOS_MOLECULE_HOME}/remaster/standard_grub.cfg"
@@ -20,16 +38,16 @@ elif [ "${remaster_type}" = "ServerBase" ]; then
 	echo "ServerBase trigger, copying server kernel over"
 	boot_kernel=$(find "${CHROOT_DIR}/boot" -name "kernel-*" | sort | head -n 1)
 	boot_ramfs=$(find "${CHROOT_DIR}/boot" -name "initramfs-*" | sort | head -n 1)
-	cp "${boot_kernel}" "${CDROOT_DIR}/boot/sabayon" || exit 1
-	cp "${boot_ramfs}" "${CDROOT_DIR}/boot/sabayon.igz" || exit 1
+	cp "${boot_kernel}" "${CDROOT_DIR}/boot/rogentos" || exit 1
+	cp "${boot_ramfs}" "${CDROOT_DIR}/boot/rogentios.igz" || exit 1
 	isolinux_source="${ROGENTOS_MOLECULE_HOME}/remaster/serverbase_isolinux.cfg"
 	grub_source="${ROGENTOS_MOLECULE_HOME}/remaster/serverbase_grub.cfg"
 elif [ "${remaster_type}" = "HardenedServer" ]; then
 	echo "HardenedServer trigger, copying server kernel over"
 	boot_kernel=$(find "${CHROOT_DIR}/boot" -name "kernel-*" | sort | head -n 1)
 	boot_ramfs=$(find "${CHROOT_DIR}/boot" -name "initramfs-*" | sort | head -n 1)
-	cp "${boot_kernel}" "${CDROOT_DIR}/boot/sabayon" || exit 1
-	cp "${boot_ramfs}" "${CDROOT_DIR}/boot/sabayon.igz" || exit 1
+	cp "${boot_kernel}" "${CDROOT_DIR}/boot/rogentos" || exit 1
+	cp "${boot_ramfs}" "${CDROOT_DIR}/boot/rogentos.igz" || exit 1
 	isolinux_source="${ROGENTOS_MOLECULE_HOME}/remaster/hardenedserver_isolinux.cfg"
 	grub_source="${ROGENTOS_MOLECULE_HOME}/remaster/hardenedserver_grub.cfg"
 fi
@@ -64,20 +82,28 @@ fi
 sed -i "s/__KMS__/${kms_string}/g" "${isolinux_destination}" || exit 1
 sed -i "s/__KMS__/${kms_string}/g" "${grub_destination}" || exit 1
 
-sabayon_pkgs_file="${CHROOT_DIR}/etc/rogentos-pkglist"
-if [ -f "${sabayon_pkgs_file}" ]; then
-	cp "${sabayon_pkgs_file}" "${CDROOT_DIR}/pkglist"
+rogentos_pkgs_file="${CHROOT_DIR}/etc/rogentos-pkglist"
+if [ -f "${rogentos_pkgs_file}" ]; then
+	cp "${rogentos_pkgs_file}" "${CDROOT_DIR}/pkglist"
 	if [ -n "${ISO_PATH}" ]; then # molecule 0.9.6 required
 		# copy pkglist over to ISO path + pkglist
-		cp "${sabayon_pkgs_file}" "${ISO_PATH}".pkglist
+		cp "${rogentos_pkgs_file}" "${ISO_PATH}".pkglist
 	fi
 fi
 
 # copy back.jpg to proper location
-isolinux_img="${CHROOT_DIR}/usr/share/backgrounds/isolinux/back.jpg"
+isolinux_img="/sabayon/boot/core/isolinux/back.jpg"
+syslinux_img="/sabayon/boot/core/syslinux/back.jpg"
 if [ -f "${isolinux_img}" ]; then
-	cp "${isolinux_img}" "${CDROOT_DIR}/isolinux/" || exit 1
+        cp "${isolinux_img}" "${CDROOT_DIR}/isolinux/" || exit 1
+        cp "${syslinux_img}" "${CDROOT_DIR}/syslinux/" || exit 1
 fi
+
+rm "${CDROOT_DIR}"/sabayon
+rm "${CDROOT_DIR}"/sabayon.igz
+rm "${CDROOT_DIR}"/boot/sabayon
+rm "${CDROOT_DIR}"/boot/sabayon.igz
+
 
 # Generate livecd.squashfs.md5
 "${ROGENTOS_MOLECULE_HOME}"/scripts/pre_iso_script_livecd_hash.sh
