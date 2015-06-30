076e282f070eefc567f620f731a6e57b8f39d880
BlackNoxis <steven.darklight@gmail.com>
2015-06-30 19:45:58 +0300

    [scripts/generic_pre_iso] adaugat nucleu live si pentru restul DE-lor

--- scripts/generic_pre_iso_script.sh
+++ scripts/generic_pre_iso_script.sh
@@ -56,8 +56,8 @@ boot_ramfs=$(find "${CHROOT_DIR}/boot" -name "initramfs-genkernel-*" | sort | he
 if [ -n "${boot_kernel}" ] && [ -f "${boot_kernel}" ]; then
 	#cp "${boot_kernel}" "${CDROOT_DIR}/boot/kogaion" || exit 1
 	#cp "${boot_ramfs}" "${CDROOT_DIR}/boot/kogaion.igz" || exit 1
-	cp "${KOGAION_MOLECULE_HOME}"/boot/kogaion_kernel/kogaion "${cdroot_boot_dir}"/kogaion
-	cp "${KOGAION_MOLECULE_HOME}"/boot/kogaion_kernel/kogaion.igz "${cdroot_boot_dir}"/kogaion.igz || exit 1
+	cp "${KOGAION_MOLECULE_HOME}"/boot/kogaion_kernel/live-brrc "${cdroot_boot_dir}"/kogaion
+	cp "${KOGAION_MOLECULE_HOME}"/boot/kogaion_kernel/live-brrc.igz "${cdroot_boot_dir}"/kogaion.igz || exit 1
 fi
 
 if [ "${remaster_type}" = "KDE" ] || [ "${remaster_type}" = "GNOME" ] || [ "${remaster_type}" = "MATE" ] || [ "${remaster_type}" = "XFCE" ] ; then
